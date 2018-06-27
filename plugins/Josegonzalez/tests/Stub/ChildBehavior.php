7407cdbbecd67d8fed246bcf6588365427fbd3b2
scpcrea <g.larroque@groupescp.fr>
2018-06-27 11:35:33 +0200

    Cleaning dependencies

--- plugins/Josegonzalez/tests/Stub/ChildBehavior.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Josegonzalez\Upload\Test\Stub;
-
-use Josegonzalez\Upload\Model\Behavior\UploadBehavior;
-
-class ChildBehavior extends UploadBehavior
-{
-    protected $_defaultConfig = ['key' => 'value'];
-}
